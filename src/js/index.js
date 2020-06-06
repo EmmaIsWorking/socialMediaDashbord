ec85326c75d9131f4c7a52eadea07442d18368c7
EmmaIsWorking <dimple.pleod@gmail.com>
2020-06-06 14:17:36 -0500

    responsive layout and designs

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const checkbox = document.getElementById('input');
 checkbox.checked = true;
 
-checkbox.addEventListener('change', function() {
+checkbox.addEventListener('change', function () {
   if (this.checked) {
     checkbox.setAttribute('checked', 'false');
     body.setAttribute('data-theme', 'light');
